7d4b570aca8120ac729b570c9801caa329f05fcd
djreillo <djreillo@gmail.com>
2023-10-01 01:49:08 +0800

    minor changes

--- src/app/aboutme/page.tsx
+++ src/app/aboutme/page.tsx
@@ -5,20 +5,21 @@
 const AboutMePage: React.FC = () => {
   return (
     <main className="flex flex-col justify-between items-center h-[93vh] min-h-[750px] w-[100%] bg-cust-image-mainpage bg-cust-size-mainpage animate-cust-animation-mainpage selection:bg-none">
-      <section className="w-full sm:w-[90vw] md:w-[80vw] lg:w-[70vw] xl:w-[50vw] 2xl:w-[40vw] min-h-[500px] mt-4 text-center mx-6 [&>p]:mx-4 [&>p]:text-white">
-        <h2 className="text-mainfg text-center mb-1 text-2xl font-semibold">About me</h2>
+      <section className="w-full sm:w-[90vw] md:w-[80vw] lg:w-[70vw] xl:w-[50vw] 2xl:w-[40vw] min-h-[500px] mt-4 text-center mx-6 [&>p]:mx-4 [&>p]:text-white [&>p]:cursor-default">
+        <h2 className="text-mainfg text-center mb-1 text-2xl font-semibold cursor-default">About me</h2>
         <p>My name is Daryll Reillo. </p>
         <br />
-        <p>Ever since I was young, I&apos;ve always liked to learn more about computers. </p>
-        <br />
-        {/* <p>This is me I got my first PC. Too bad I eventually destroyed it on one of my experiments.😊</p>
-        <br /> */}
         <p>
-          I left my career of 10+ years as an SAP Functional Consultant so I can pursue my dream career on web development. Yes, it took me a decade to realize
-          I wasn&apos;t really happy in my career path which led me to this big change.
+          Ever since I was young, I&apos;ve always liked computers in general. I remember playing video games all day on my 16-bit Sega Mega Drive (a.k.a Sega Genesis) back when I was just seven. In high school, I remember trying to overclock my first PC until it overheated and died. Now, the tech has advanced so much, by leaps and bounds, that we can never bring back those low-tech days again.
         </p>
         <br />
-        {/* <p>Thank you and have a nice day ahead! </p> */}
+        <p>
+          Anyway, I recently left my 10+ year-career as an SAP Functional Consultant so that I can pursue my dream career on web development. Yes, it took me a decade to actually take a risk for this. I have no job as of right now, but I am doing what I like.
+        </p>
+        <br />
+        <p>
+          Kudos to all of the people who believed and supported me - Giezzel, Alp, Merbs, Ralph, and all of you. I love you all.
+        </p>
       </section>
     </main>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 
 import StackList from './_components/projectInfo/StackList';
 import ProjectLinks from './_components/projectInfo/ProjectLinks';
-import myPic from '@/app/_images/my_pic/20220910_124833.jpg'
+import myPic from '@/app/_images/my_pic/20220910_124833.jpg';
 
 const Homepage: React.FC = () => {
   return (
@@ -13,14 +13,13 @@ const Homepage: React.FC = () => {
       <section className="w-full sm:w-[90vw] md:w-[80vw] lg:w-[70vw] xl:w-[50vw] 2xl:w-[40vw] min-h-[500px] mt-4 text-center mx-6 [&>p]:mx-4 [&>p]:text-white">
         <h2 className="text-mainfg text-center mb-1 text-2xl font-semibold">Hi there!</h2>
         <p>I&apos;m Daryll, an aspiring web developer. </p>
-        <Image src={myPic} alt="the developer's portrait" width={150} className='rounded-md mx-auto my-2'/>
-        <p>I left my career of 10+ years as an SAP Functional Consultant so I can pursue my dream career on web development.</p>
+        <Image src={myPic} alt="the developer's portrait" width={150} className="rounded-md mx-auto my-2" />
         <br />
-        <p>Please check out the apps by clicking on menu on the upper right.</p>
+        <p>Please check out my apps by clicking on the menu button on the upper right.</p>
         <br />
         <p>
-          All apps are public and free to use, regardless of your sign in status. <br />
-          Thank you and have a great day ahead!
+          All apps in this site are public and free to use, regardless if you login or not. <br />
+          Thanks and see you later!
         </p>
       </section>
       <section className="mb-8 min-w-[320px]">
